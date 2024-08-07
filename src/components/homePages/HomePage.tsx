a8b2258f66fa1770ed85613a761ea8eff91efbd0
Sopnil Swapon <sopnilswapon1@gmail.com>
2024-08-07 13:01:36 +0600

    added a navbar

---
+++ src/components/homePages/HomePage.tsx
@@ -0,0 +1,14 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <Typography component='p' variant='body1'>
+                This is homepage
+            </Typography>
+        </div>
+    );
+};
+
+export default HomePage;
